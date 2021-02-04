c1138137a68407b0f424a2742f252817f1a3bb39
TomazSouza <developer.software.ti@gmail.com>
2021-02-04 09:35:59 -0300

    first commit

---
+++ app/src/main/java/com/example/kfeedreader/MainActivity.kt
@@ -0,0 +1,11 @@
+tpackage com.example.kfeedreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
